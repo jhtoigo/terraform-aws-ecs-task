5cbb34ef7b79a633df599ee7c8fc2b5a274e067d
Joao Henrique Toigo <jhtoigo@acad.ftec.com.br>
2024-08-12 11:42:16 -0300

    remove unused outputs

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,3 @@ output "task_definition_arn" {
   description = "ARN of the ECS task definition"
   value       = aws_ecs_task_definition.ecs_task.arn
 }
-
-output "arn_values" {
-  value = local.secrets_arns
-}
-
-output "container_secrets_values" {
-  value = var.container_secrets
-}
